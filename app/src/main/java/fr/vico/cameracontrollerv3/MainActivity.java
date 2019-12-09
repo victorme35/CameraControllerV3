3c72d0872cd7f59bd8613c0627694801f23e106e
victorme35 <victor.menard@neuf.fr>
2019-12-09 09:58:16 +0100

    Initial commit

---
+++ app/src/main/java/fr/vico/cameracontrollerv3/MainActivity.java
@@ -0,0 +1,65 @@
+package fr.vico.cameracontrollerv3;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.text.Html;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    public WebView webView;
+
+    // Création de l'activité
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Instanciation du WebView
+        webView = (WebView) findViewById(R.id.webview);
+
+        // Effacement du cache
+        webView.clearCache(true);
+        // suppression du stockage de données dans le cache
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+        // Information temporaire de test(à supprimer à la convenance du développeur)
+        myToast("Chargement de la page web");
+
+        // Chargement de la page web
+        webView.loadUrl(getString(R.string.page_AccesSite));
+    } // fin de onCreate
+
+    @Override
+    protected void onStart()    { super.onStart();}
+    @Override
+    protected void onRestart()  { super.onRestart();}
+    @Override
+    protected void onResume()   { super.onResume();}
+    @Override
+    protected void onPause()    { super.onPause();}
+    @Override
+    protected void onStop()     { super.onStop();}
+    @Override
+    protected void onDestroy()  { super.onDestroy();}
+
+    //_____________________________________________________________________
+    // Classes filles
+    //_____________________________________________________________________
+
+    //_____________________________________________________________________
+    // Méthodes filles
+    //_____________________________________________________________________
+
+    // Méthode de personnalisation des informations du Toast
+
+    public void myToast(String theMessage){
+        Toast toast = Toast.makeText(getApplicationContext(), Html.fromHtml(theMessage), Toast.LENGTH_SHORT);
+        // Affichage du toast
+        toast.show();
+    }
+
+} // Fin class TestWebview
+
